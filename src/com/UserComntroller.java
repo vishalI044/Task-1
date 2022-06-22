481ae05e9a8fabf8225830647c42a610e3b34a80
Vishal R <vishal.r@msf.ad>
2022-06-22 11:07:18 +0530

    first commit

--- src/com/UserComntroller.java
+++ src/com/UserComntroller.java
@@ -3,23 +3,23 @@ package com;
 import java.util.Scanner;
 
 public class UserComntroller {
-	public static void main(String []args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("enter employee Departmanent ID 1. permanent | 2. part Time | 3. contract");
-		int employee_Did = scan.nextInt();
-		
-		if(employee_Did == 1) {
-			Employee employe = new PermanentEmployee();
-			PermanentEmployee.employeeDetails();
-		}
-		else if(employee_Did == 2) {
-			Employee employee = new PartTimeEmployee();
-			PartTimeEmployee.employeeDetails();
-		}
-		else if(employee_Did == 3) {
-			Employee employee = new ContractEmployee();			
-			ContractEmployee.employeeDetails();
-		}
-				
-	}
+//	public static void main(String []args) {
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("enter employee Departmanent ID 1. permanent | 2. part Time | 3. contract");
+//		int employee_Did = scan.nextInt();
+//		
+//		if(employee_Did == 1) {
+//			Employee employe = new PermanentEmployee();
+//			PermanentEmployee.employeeDetails();
+//		}
+//		else if(employee_Did == 2) {
+//			Employee employee = new PartTimeEmployee();
+//			PartTimeEmployee.employeeDetails();
+//		}
+//		else if(employee_Did == 3) {
+//			Employee employee = new ContractEmployee();			
+//			ContractEmployee.employeeDetails();
+//		}
+//				
+//	}
 }
